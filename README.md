c126c90563d463ec2c66632e011b8bcff1347b6d
Monty Rasmussen <montyr75@gmail.com>
2013-12-07 17:24:31 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Message Cipher
+======
+
+[Reddit Daily Programmer exercise 003 (intermediate)](http://www.reddit.com/r/dailyprogrammer/comments/pkwb1/2112012_challenge_3_intermediate/)
+
+A Polymer Dart application that translates messages using a customizable (in code) cipher.
---
+++ web/views/main/main_view.dart
@@ -0,0 +1,48 @@
+library main_view;
+
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+
+@CustomTag('main-view')
+class MainView extends PolymerElement {
+
+  @observable String originalMessage = "";
+  @observable String encryptedMessage = "";
+
+  // non-visual initialization can be done here
+  MainView.created() : super.created();
+
+  // other initialization can be done here
+  @override void enteredView() {
+    super.enteredView();
+    print("MainView::enteredView()");
+  }
+
+  // algorithm used to encode one character
+  int encodeCodeUnit(int codeUnit) {
+    return codeUnit + 1;
+  }
+
+  // algorithm used to decode one character
+  int decodeCodeUnit(int codeUnit) {
+    return codeUnit - 1;
+  }
+
+  void onEncrypt(Event event, var detail, Element target) {
+    print("MainView::onEncrypt()");
+
+    encryptedMessage = alter(originalMessage, operation: (int codeUnit) => codeUnit + 1);
+  }
+
+  void onDecrypt(Event event, var detail, Element target) {
+    print("MainView::onDecrypt()");
+
+    originalMessage = alter(encryptedMessage, operation: decodeCodeUnit);
+  }
+
+  String alter(String msg, {Function operation}) => new String.fromCharCodes(msg.codeUnits.map(operation));
+
+  // this lets the global CSS bleed through into the Shadow DOM of this element
+  bool get applyAuthorStyles => true;
+}
+
